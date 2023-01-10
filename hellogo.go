26d260678560e23422d0b16edb05c2859ea77868
TaichiOhmi <o6mi5.ta1@gmail.com>
2023-01-10 17:46:00 +0900

    math

--- hellogo.go
+++ hellogo.go
@@ -109,4 +109,40 @@ func main() {
 	pl(now.Year(), now.Month(), now.Day())
 	pl(now.Hour(), now.Minute(), now.Second())
 	*/
+
+	/* math
+	pl("5 + 4 = ", 5+4)
+	pl("5 - 4 = ", 5-4)
+	pl("5 * 4 = ", 5*4)
+	pl("5 / 4 = ", 5/4)
+	pl("5 % 4 = ", 5%4)
+	mInt := 1
+	mInt++ // == mInt += 1, mInt = mInt + 1
+	pl(mInt)
+	pl("Float Precision =", 0.111111111111111111+0.111111111111111111)
+	seedSecs := time.Now().Unix()
+	rand.Seed(seedSecs) // これを固定すれば同じ値が出る。
+	pl(seedSecs)
+	randNum := rand.Intn(50) + 1 // Intn は 0から引数までの間でランダム値を生成する。
+	pl("Random :", randNum)
+	pl("Abs(-10 =", math.Abs(-10))
+	pl("Pow(4, 2) =", math.Pow(4, 2))
+	pl("Sqrt(16) =", math.Sqrt(16))
+	pl("Cbrt(8) =", math.Cbrt(8))
+	pl("Ceil(4.4) =", math.Ceil(4.4))
+	pl("Floor(4.4) =", math.Floor(4.4))
+	pl("Roud(4.4) =", math.Round(4.4))
+	pl("Log2(8) =", math.Log2(8))
+	pl("Log10(100) =", math.Log10(100))
+	// eの2乗の対数をとる
+	pl("Log(7.389) =", math.Log(math.Exp(2)))
+	pl("Max(5,4) =", math.Max(5, 4))
+	pl("Min(5,4) =", math.Min(5, 4))
+	// 90度をラジアンに変換する
+	r90 := 90 * math.Pi / 180
+	d90 := r90 * (180 / math.Pi)
+	fmt.Printf("%.2f radians = %.2f degrees\n", r90, d90)
+	pl("Sin(90) =", math.Sin(r90))
+	// Cos, Tan, Acos, Asin, Atan, Asinh, Acosh, Atanh, Atan2, Cosh, Sinh, Sincos, Htpot, etc..
+	*/
 }
