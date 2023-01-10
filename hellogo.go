3a5cc3c6a69328f66a2279d339a1c5fb7ac98999
TaichiOhmi <o6mi5.ta1@gmail.com>
2023-01-10 18:52:24 +0900

    arrays

--- hellogo.go
+++ hellogo.go
@@ -224,4 +224,39 @@ func main() {
 		pl(num)
 	}
 	*/
+
+	/* arrays
+	var arr1 [5]int
+	arr1[0] = 1
+
+	arr2 := [5]int{1, 2, 3, 4, 5}
+	pl("Index 0 :", arr2[0])
+	pl("Arr Length :", len(arr2))
+	for i := 0; i < len(arr2); i++ {
+		pl(arr2[i])
+	}
+	for i, v := range arr2 {
+		fmt.Printf("index: %d, value:%d\n", i, v)
+	}
+	arr3 := [2][2]int{
+		{1, 2},
+		{3, 4},
+	}
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			fmt.Printf("arr3[%d][%d] = %d\n", i, j, arr3[i][j])
+		}
+	}
+
+	aStr1 := "abcde"
+	rArr := []rune(aStr1)
+	pl(rArr) // [97 98 99 100 101]
+	for _, v := range rArr {
+		fmt.Printf("Rune Array : %d\n", v)
+	}
+	byteArr := []byte{'a', 'b', 'c'}
+	pl(byteArr)
+	bStr := string(byteArr[:])
+	pl("I'm a string :", bStr)
+	*/
 }
