0b15d34ab0c087f5e7d4b1058726183d62d11684
TaichiOhmi <o6mi5.ta1@gmail.com>
2023-01-10 16:36:00 +0900

    variables

--- hellogo.go
+++ hellogo.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
 )
 
 var pl = fmt.Println
 
 func main() {
+	/* userInput
 	pl("Hello Go")
 	pl("What is your name?")
 	reader := bufio.NewReader(os.Stdin)
@@ -19,4 +17,17 @@ func main() {
 	} else {
 		log.Fatal(err)
 	}
+	*/
+
+	/* variables
+	// var <name> <type>
+	// 変数名や関数名、型名が大文字で始まる場合、エクスポートすることを表す。
+	// 基本の変数名はキャメルケース
+	// var vName string = "Taichi" // 型を明示
+	// var v1, v2 = 1.2, 3.4       //
+	// var v3 = "hello"
+	// v4 := 2.4
+	// v4 = 5.4
+	*/
+
 }
