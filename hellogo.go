00cb1ced832810e019072de97e649c24dd73c9ec
TaichiOhmi <o6mi5.ta1@gmail.com>
2023-01-11 10:38:41 +0900

    file IO

--- hellogo.go
+++ hellogo.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"os"
 )
 
 var pl = fmt.Println
@@ -50,6 +53,7 @@ func getArraySum(arr []int) int {
 }
 */
 
+/*
 // pointers
 func changeValue(myPtr *int) {
 	*myPtr = 12
@@ -69,6 +73,7 @@ func getAverage(nums ...float64) float64 {
 	}
 	return (sum / NumSize)
 }
+*/
 
 func main() {
 	/* userInput
@@ -392,10 +397,74 @@ func main() {
 	pl("f4 after func :", f4)
 	*/
 
-	/* pass array pointers */
+	/* pass array pointers
 	pArr := [4]int{1, 2, 3, 4}
 	dblArrVals(&pArr)
 	pl(pArr)
 	iSlice := []float64{11, 13, 17}
 	fmt.Printf("Average : %.3f\n", getAverage(iSlice...))
+	*/
+
+	/* file IO
+	f, err := os.Create("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	iPrimeArr := []int{2, 3, 5, 7, 11}
+	var sPrimeArr []string
+	for _, i := range iPrimeArr {
+		sPrimeArr = append(sPrimeArr, strconv.Itoa(i))
+	}
+	for _, num := range sPrimeArr {
+		_, err := f.WriteString(num + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	f, err = os.Open("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	scan1 := bufio.NewScanner(f)
+	for scan1.Scan() {
+		pl("Prime :", scan1.Text())
+	}
+	if err := scan1.Err(); err != nil {
+		log.Fatal(err)
+	}
+	*/
+	/*
+		Exactly one of O_RDONLY, O_WRONLY, or O_RDWR must be specified
+
+		O_RDONLY : open the file read-only
+		O_WRONLY : open the file write-only
+		O_RDWR : open the file read-write
+
+		These can be or'ed
+
+		O_APPEND : append data to the file when writing
+		O_CREATE : create a new file if none exists
+		O_EXCL : used with O_CREATE, file must not exist
+		O_SYNC : open for synchronous I/O
+		O_TRUNC : truncate regular writable file when opened
+	*/
+
+	_, err := os.Stat("data.txt")
+	// err が file not exist の時、
+	if errors.Is(err, os.ErrNotExist) {
+		pl("File Doesn't Exist")
+	} else {
+		f, err := os.OpenFile("data.txt",
+			os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		if _, err := f.WriteString("13\n"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
 }
